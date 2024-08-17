ac19ce014ecfc66d0149b944c863296ac138afd6
Aurelien Borst <aurelien.borst@gmail.com>
2024-08-17 15:49:57 +0200

    add strapi-plugin-transformer

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,27 @@
-export default () => ({});
+export default ({ env }) => ({
+    // upload: {
+    //   config: {
+    //     provider: "cloudinary",
+    //     providerOptions: {
+    //       cloud_name: env("CLOUDINARY_NAME"),
+    //       api_key: env("CLOUDINARY_KEY"),
+    //       api_secret: env("CLOUDINARY_SECRET"),
+    //     },
+    //   },
+    // },
+    // wysiwyg: {
+    //   enabled: false,
+    //   resolve: "./src/plugins/wysiwyg",
+    // },
+  
+    transformer: {
+      enabled: true,
+      config: {
+        responseTransforms: {
+          removeAttributesKey: true,
+          removeDataKey: true,
+        },
+      },
+    },
+  });
+  
