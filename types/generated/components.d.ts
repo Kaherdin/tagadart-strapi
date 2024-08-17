00a4111a157fa4c18f0ff9087a1e0513590c9a79
Aurelien Borst <aurelien.borst@gmail.com>
2024-08-17 14:46:36 +0200

    -add: Projects collection -add: Components button, cta, pageIntro and testi

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,5 +1,67 @@
 import type { Schema, Attribute } from '@strapi/strapi';
 
+export interface SectionTestimonials extends Schema.Component {
+  collectionName: 'components_section_testimonials';
+  info: {
+    displayName: 'Testimonials';
+    icon: 'server';
+  };
+  attributes: {
+    content: Attribute.RichText;
+    avatar: Attribute.Media<'images' | 'files' | 'videos' | 'audios'>;
+    name: Attribute.String;
+  };
+}
+
+export interface SectionPageIntro extends Schema.Component {
+  collectionName: 'components_section_page_intros';
+  info: {
+    displayName: 'Page Intro';
+    icon: 'house';
+  };
+  attributes: {
+    title: Attribute.String;
+    eyebrow: Attribute.String;
+    description: Attribute.RichText;
+    cover: Attribute.Media<'images' | 'files' | 'videos' | 'audios'>;
+  };
+}
+
+export interface SectionCta extends Schema.Component {
+  collectionName: 'components_section_ctas';
+  info: {
+    displayName: 'CTA';
+    icon: 'cursor';
+  };
+  attributes: {
+    title: Attribute.String;
+    eyebrow: Attribute.String;
+    contentadasd: Attribute.String;
+    content: Attribute.RichText;
+    Buttons: Attribute.Component<'elements.button', true>;
+    cover: Attribute.Media<'images' | 'files' | 'videos' | 'audios'>;
+  };
+}
+
+export interface ElementsButton extends Schema.Component {
+  collectionName: 'components_elements_buttons';
+  info: {
+    displayName: 'Button';
+  };
+  attributes: {
+    link: Attribute.String;
+    type: Attribute.Enumeration<['primary', 'secondary']>;
+    text: Attribute.String;
+  };
+}
+
 declare module '@strapi/types' {
-  export module Shared {}
+  export module Shared {
+    export interface Components {
+      'section.testimonials': SectionTestimonials;
+      'section.page-intro': SectionPageIntro;
+      'section.cta': SectionCta;
+      'elements.button': ElementsButton;
+    }
+  }
 }
--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -362,6 +362,43 @@ export interface AdminTransferTokenPermission extends Schema.CollectionType {
   };
 }
 
+export interface ApiProjectProject extends Schema.CollectionType {
+  collectionName: 'projects';
+  info: {
+    singularName: 'project';
+    pluralName: 'projects';
+    displayName: 'Project';
+  };
+  options: {
+    draftAndPublish: true;
+  };
+  attributes: {
+    year: Attribute.String;
+    client: Attribute.String;
+    service: Attribute.String;
+    user: Attribute.Relation<'api::project.project', 'oneToOne', 'admin::user'>;
+    expertise: Attribute.RichText;
+    link: Attribute.String;
+    PageIntro: Attribute.Component<'section.page-intro'>;
+    Testimonials: Attribute.Component<'section.testimonials', true>;
+    createdAt: Attribute.DateTime;
+    updatedAt: Attribute.DateTime;
+    publishedAt: Attribute.DateTime;
+    createdBy: Attribute.Relation<
+      'api::project.project',
+      'oneToOne',
+      'admin::user'
+    > &
+      Attribute.Private;
+    updatedBy: Attribute.Relation<
+      'api::project.project',
+      'oneToOne',
+      'admin::user'
+    > &
+      Attribute.Private;
+  };
+}
+
 export interface PluginUploadFile extends Schema.CollectionType {
   collectionName: 'files';
   info: {
@@ -798,6 +835,7 @@ declare module '@strapi/types' {
       'admin::api-token-permission': AdminApiTokenPermission;
       'admin::transfer-token': AdminTransferToken;
       'admin::transfer-token-permission': AdminTransferTokenPermission;
+      'api::project.project': ApiProjectProject;
       'plugin::upload.file': PluginUploadFile;
       'plugin::upload.folder': PluginUploadFolder;
       'plugin::content-releases.release': PluginContentReleasesRelease;
