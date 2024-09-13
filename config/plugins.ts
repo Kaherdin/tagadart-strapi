f2d0a67532c100d2723f69b6f10c594345bb59fe
Aurelien Borst <aurelien.borst@gmail.com>
2024-09-13 17:00:23 +0200

    install resend plugin

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,27 +1,38 @@
 export default ({ env }) => ({
-    upload: {
-      config: {
-        provider: "cloudinary",
-        providerOptions: {
-          cloud_name: env("CLOUDINARY_NAME"),
-          api_key: env("CLOUDINARY_KEY"),
-          api_secret: env("CLOUDINARY_SECRET"),
-        },
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
       },
     },
-    // wysiwyg: {
-    //   enabled: false,
-    //   resolve: "./src/plugins/wysiwyg",
-    // },
-  
-    transformer: {
-      enabled: true,
-      config: {
-        responseTransforms: {
-          removeAttributesKey: true,
-          removeDataKey: true,
-        },
+  },
+  // wysiwyg: {
+  //   enabled: false,
+  //   resolve: "./src/plugins/wysiwyg",
+  // },
+  email: {
+    config: {
+      provider: "strapi-provider-email-resend",
+      providerOptions: {
+        apiKey: env("RESEND_API_KEY"), // Required
+      },
+      settings: {
+        defaultFrom: "aurelien@tagadart.com",
+        defaultReplyTo: "aurelien@tagadart.com",
       },
     },
-  });
-  
+  },
+
+  transformer: {
+    enabled: true,
+    config: {
+      responseTransforms: {
+        removeAttributesKey: true,
+        removeDataKey: true,
+      },
+    },
+  },
+});
