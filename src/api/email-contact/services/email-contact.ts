2622b3e8f873cae9e5dce0c68ddde7ab1ce911d1
Aurelien Borst <aurelien.borst@gmail.com>
2024-09-14 04:51:08 +0200

    works

--- src/api/email-contact/services/email-contact.ts
+++ src/api/email-contact/services/email-contact.ts
@@ -9,26 +9,26 @@ const fs = require("fs");
 export default {
   emailContactService: async (ctx) => {
     try {
-      // if (!ctx.request.body.data) {
-      //   console.log(ctx.request.body);
-      //   ctx.status = 400;
-      //   ctx.body = { message: "Missing required fields data" };
-      //   return ctx.body;
-      // }
+      if (!ctx.request.body.data) {
+        ctx.status = 400;
+        ctx.body = { message: "Missing required fields data" };
+        return ctx.body;
+      }
 
       // const { input, emailTo } =
       //   typeof ctx.request.body.data === "string"
       //     ? JSON.parse(ctx.request.body.data)
       //     : ctx.request.body.data;
 
-      const data = parseBody(ctx).data;
+      // const data = parseBody(ctx).data;
 
       //Not Multipart
-      // const data =
-      //   typeof ctx.request.body.data === "string"
-      //     ? JSON.parse(ctx.request.body.data)
-      //     : ctx.request.body.data;
-      // console.log(data, "data");
+      const data =
+        typeof ctx.request.body.data === "string"
+          ? parseBody(ctx).data
+          : ctx.request.body.data;
+      console.log(data, "data");
+      console.log(typeof data, "data type");
 
       //Attachements
       // let attachements = [];
