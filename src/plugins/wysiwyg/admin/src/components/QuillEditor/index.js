24ea8eeabbabb4d0f3a45cc5874cfd47b21f3548
Aurélien Linux Borst <aurelien.borst@gmail.com>
2024-11-07 18:15:19 +0100

    add wysiwyg plutgin quill

---
+++ src/plugins/wysiwyg/admin/src/components/QuillEditor/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+const Editor = ({ onChange, name, value }) => {
+  const modules = {
+    toolbar: [
+      [{ header: [1, 2, 3, 4, 5, 6] }],
+      [{ size: [] }],
+      ["bold", "italic", "underline", "strike"],
+      [{ list: "ordered" }, { list: "bullet" }],
+      ["link"],
+      ["clean"],
+    ],
+  };
+  return (
+    <ReactQuill
+      theme="snow"
+      value={value}
+      modules={modules}
+      onChange={(content, event, editor) => {
+        onChange({ target: { name, value: content } });
+      }}
+    />
+  );
+};
+export default Editor;
---
+++ src/plugins/wysiwyg/admin/src/index.tsx
@@ -0,0 +1,79 @@
+import pluginPkg from "../../package.json";
+import pluginId from "./pluginId";
+import Initializer from "./components/Initializer";
+
+// @ts-ignore //TODO: Add ts
+import Wysiwyg from "./components/Wysiwyg";
+
+const name = pluginPkg.strapi.name;
+
+export default {
+  register(app: any) {
+    app.registerPlugin({
+      id: pluginId,
+      initializer: Initializer,
+      isReady: false,
+      name,
+    });
+    app.addFields({ type: "wysiwyg", Component: Wysiwyg });
+  },
+};
+
+// export default {
+//   register(app: any) {
+//     app.addMenuLink({
+//       to: `/plugins/${pluginId}`,
+//       icon: PluginIcon,
+//       intlLabel: {
+//         id: `${pluginId}.plugin.name`,
+//         defaultMessage: name,
+//       },
+//       Component: async () => {
+//         const component = await import('./pages/App');
+
+//         return component;
+//       },
+//       permissions: [
+//         // Uncomment to set the permissions of the plugin here
+//         // {
+//         //   action: '', // the action name should be plugin::plugin-name.actionType
+//         //   subject: null,
+//         // },
+//       ],
+//     });
+//     const plugin = {
+//       id: pluginId,
+//       initializer: Initializer,
+//       isReady: false,
+//       name,
+//     };
+
+//     app.registerPlugin(plugin);
+//   },
+
+//   bootstrap(app: any) {},
+
+//   async registerTrads(app: any) {
+//     const { locales } = app;
+
+//     const importedTrads = await Promise.all(
+//       (locales as any[]).map((locale) => {
+//         return import(`./translations/${locale}.json`)
+//           .then(({ default: data }) => {
+//             return {
+//               data: prefixPluginTranslations(data, pluginId),
+//               locale,
+//             };
+//           })
+//           .catch(() => {
+//             return {
+//               data: {},
+//               locale,
+//             };
+//           });
+//       })
+//     );
+
+//     return Promise.resolve(importedTrads);
+//   },
+// };
